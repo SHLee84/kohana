99dc57685bc9ed9b24e3c8d492f32830446d1e12
SHLee84 <sanghyun.lee@shlee.ca>
2015-06-11 23:47:19 -0400

    Part 1
    - tested with microtime to determine the fastest solution
    - need to change htaccess for appropriate routing

---
+++ application/classes/Controller/Part1.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Part1 extends Controller {
+
+	public function action_index()
+	{
+		$text = '';
+		for ($i = 1; $i < 101; $i++)
+		{
+			$text .=$i.': ';
+			if ($i % 15 == 0)
+			{
+				$text .='fizzbuzz';
+			}
+			else if ($i % 3 == 0)
+			{
+				$text .='fizz';
+			}
+			else if ($i % 5 == 0)
+			{
+				$text .='buzz';
+			}
+			else
+			{
+				$text .=$i;
+			}
+			$text .='<br />';
+		}
+		$view = View::factory('part1')->set('content', $text);
+		$this->response->body($view);
+	}
+
+}
---
+++ application/views/part1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title><?php echo 'Part 1'; ?></title>
+	</head>
+	<body>
+		<a href="index.php/<?php echo 'contest'; ?>"><?php echo 'contest'; ?></a>
+		<h1><?php echo 'This is FIZZBUZZ'; ?></h1>
+		<p><?php echo $content; ?></p>
+	</body>
+</html>
